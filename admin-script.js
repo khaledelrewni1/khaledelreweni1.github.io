a0e187fa55a6d41e5b36350f4cc02266ef86e351
khaledelrewni1 <khaledelrewni@gmail.com>
2025-10-10 18:32:07 +0300

    Update admin-script.js

--- admin-script.js
+++ admin-script.js
@@ -1,7 +1,7 @@
 let scheduleData = JSON.parse(localStorage.getItem('scheduleData')) || [ /* نفس البيانات من script.js */ ];
 let adminCalendar;
 
-const adminPassword = 'admin123';  // غيرها
+const adminPassword = 'admin';  // غيرها
 const adminUser = 'admin';
 
 document.addEventListener('DOMContentLoaded', function() {
